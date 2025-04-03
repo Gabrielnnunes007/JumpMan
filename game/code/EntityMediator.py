826e021facd6ae7fadcf953284c0caca615c805b
Gabriel_Nunes <gabsgab1234@gmail.com>
2025-04-03 05:00:13 -0300

    Adding the estruture of collisions in method EntityMediator

--- game/code/EntityMediator.py
+++ game/code/EntityMediator.py
@@ -1,5 +1,6 @@
 from game.code.Enemy import Enemy
 from game.code.Entity import Entity
+from game.code.Player import Player
 
 
 class EntityMediator:
@@ -13,8 +14,11 @@ class EntityMediator:
     @staticmethod
     def verify_collision(entity_list: list[Entity]):
         for i in range(len(entity_list)):
-            test_entity = entity_list[i]
-            EntityMediator.__verify_collision_window(test_entity)
+            entity1 = entity_list[i]
+            EntityMediator.__verify_collision_window(entity1)
+            for j in range(i+1, len(entity_list)):
+                entity2 = entity_list[j]
+                EntityMediator.__verify_collision_entity(entity1,entity2)
 
 
     @staticmethod
@@ -22,3 +26,15 @@ class EntityMediator:
        for ent in entity_list:
            if ent.health <= 0:
                entity_list.remove(ent)
+
+
+    @staticmethod
+    def __verify_collision_entity(ent1, ent2):
+        valid_interaction = False
+        if isinstance(ent1, Player) and isinstance(ent2, Enemy):
+            valid_interaction = True
+        elif isinstance(ent1, Enemy) and isinstance(ent2, Player):
+            valid_interaction = True
+
+        if valid_interaction: #if valid_interaction == True:
+            pass
