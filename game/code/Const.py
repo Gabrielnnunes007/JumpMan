5322f19ff077db34b5cc74536346cbbd678ff020
Gabriel_Nunes <gabsgab1234@gmail.com>
2025-04-10 00:20:46 -0300

    Changing enemys and add more levels

--- game/code/Const.py
+++ game/code/Const.py
@@ -12,34 +12,130 @@ ENTITY_SPEED = {
     'Level1bg1' : 1.6,
     'Level1bg2' : 1,
     'Level1bg3' : 1,
+
+    'Level2bg0' : 0,
+    'Level2bg1' : 1.6,
+    'Level2bg2' : 1,
+
+    'Level3bg0': 0,
+    'Level3bg1': 1.6,
+    'Level3bg2': 1,
+    'Level3bg3': 1,
+    'Level3bg4': 1,
+    'Level3bg5': 1,
+    'Level3bg6': 1,
+
+    'Level4bg0' : 1,
+    'Level4bg1' : 1.6,
+    'Level4bg2' : 1,
+    'Level4bg3' : 1,
+
+    'Level5bg0': 1,
+    'Level5bg1': 1.6,
+    'Level5bg2': 1,
+    'Level5bg3': 1,
+
+    'Level6bg0': 1,
+    'Level6bg1': 1.6,
+    'Level6bg2': 1,
+    'Level6bg3': 1,
+
     'Player1': 5,
     'Player2': 5,
     'Enemy1': 2,
     'Enemy2': 3,
+    'Enemy3': 4,
+    'Enemy4': 4,
+    'Enemy5': 4,
+    'Enemy6': 4,
 }
 ENTITY_HEALTH = {
     'Level1bg0': 999,
     'Level1bg1': 999,
     'Level1bg2': 999,
     'Level1bg3': 999,
+
+    'Level2bg0': 999,
+    'Level2bg1': 999,
+    'Level2bg2': 999,
+
+    'Level3bg0': 999,
+    'Level3bg1': 999,
+    'Level3bg2': 999,
+    'Level3bg3': 999,
+    'Level3bg4': 999,
+    'Level3bg5': 999,
+    'Level3bg6': 999,
+
+    'Level4bg0': 999,
+    'Level4bg1': 999,
+    'Level4bg2': 999,
+    'Level4bg3': 999,
+
+    'Level5bg0': 999,
+    'Level5bg1': 999,
+    'Level5bg2': 999,
+    'Level5bg3': 999,
+
+    'Level6bg0': 999,
+    'Level6bg1': 999,
+    'Level6bg2': 999,
+    'Level6bg3': 999,
+
     'Player1': 300,
     'Player2': 300,
     'Enemy1': 999,
     'Enemy2': 999,
+    'Enemy3': 999,
+    'Enemy4': 999,
+    'Enemy5': 999,
+    'Enemy6': 999,
 }
+
 ENTITY_DAMAGE = {
     'Level1bg0': 0,
     'Level1bg1': 0,
     'Level1bg2': 0,
     'Level1bg3': 0,
+
+    'Level2bg0': 0,
+    'Level2bg1': 0,
+    'Level2bg2': 0,
+
+    'Level3bg0': 0,
+    'Level3bg1': 0,
+    'Level3bg2': 0,
+    'Level3bg3': 0,
+    'Level3bg4': 0,
+    'Level3bg5': 0,
+    'Level3bg6': 0,
+
+    'Level4bg0': 0,
+    'Level4bg1': 0,
+    'Level4bg2': 0,
+    'Level4bg3': 0,
+
+    'Level5bg0': 0,
+    'Level5bg1': 0,
+    'Level5bg2': 0,
+    'Level5bg3': 0,
+
+    'Level6bg0': 0,
+    'Level6bg1': 0,
+    'Level6bg2': 0,
+    'Level6bg3': 0,
+
     'Player1': 1,
     'Player2': 1,
     'Enemy1': 25,
     'Enemy2': 25,
+    'Enemy3': 25,
+    'Enemy4': 25,
+    'Enemy5': 25,
+    'Enemy6': 25,
 }
-
 EVENT_ENEMY = pygame.USEREVENT + 1
-
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 # m
 MENU_OPTION = ('NEW GAME ',
                'NEW GAME SURVIVAL',
@@ -66,6 +162,9 @@ PLAYER_KEY_SHOOT = {
     'Player2': pygame.K_LCTRL
 }
 
+#t
+TIMEOUT_STEP = 100
+TIMEOUT_LEVEL = 10000
 # w
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- game/code/Level.py
+++ game/code/Level.py
@@ -10,24 +10,32 @@ from pygame.font import Font
 from game.code import Entity
 from game.code.EntityFactory import EntityFactory
 from game.code.EntityMediator import EntityMediator
-from game.code.Const import color_white, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, color_green, color_red
+from game.code.Const import color_white, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, color_green, color_red, EVENT_TIMEOUT, \
+    TIMEOUT_STEP, TIMEOUT_LEVEL
+from game.code.Player import Player
 
 
 class Level:
-    def __init__(self, window, name, game_mode):
+    def __init__(self, window: Surface, name: str, game_mode: str, player_score: list[int]):
         self.window = window
-        self.timeout = 20000
+        self.timeout = TIMEOUT_LEVEL
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1bg'))
-        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        self.entity_list.extend(EntityFactory.get_entity(self.name + 'bg'))
+        player = (EntityFactory.get_entity('Player1'))
+        player.score = player_score[0]
+        self.entity_list.append(player)
         if game_mode == MENU_OPTION[2]:
             self.entity_list.append(EntityFactory.get_entity('Player2'))
-        pygame.time.set_timer(EVENT_ENEMY,2000)
+            player = (EntityFactory.get_entity('Player2'))
+            player.score = player_score[1]
+            self.entity_list.append(player)
+        pygame.time.set_timer(EVENT_ENEMY,1000)
+        pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP)
 
 
-    def run(self):
+    def run(self, player_score: list[int]):
         pygame.mixer_music.load(f'./asset/game songs/levelMusic.mp3')
         somMonstro1 = pygame.mixer.Sound('./asset/game songs/monstro1.mp3')
         somMonstro2 = pygame.mixer.Sound('./asset/game songs/monstro2.mp3')
@@ -55,11 +63,41 @@ class Level:
                     sys.exit()
 
                 if event.type == EVENT_ENEMY:
-                    choice = random.choice(('Enemy2', 'Enemy1'))
+                    enemies_by_level = {
+                        'Level1': ['Enemy1', ],
+                        'Level2': ['Enemy1', 'Enemy2',],
+                        'Level3': ['Enemy1', 'Enemy2', 'Enemy3'],
+                        'Level4': ['Enemy1', 'Enemy2', 'Enemy3','Enemy4'],
+                        'Level5': ['Enemy1', 'Enemy2', 'Enemy3''Enemy4','Enemy5'],
+                        'Level6': ['Enemy1', 'Enemy2', 'Enemy3','Enemy4','Enemy5','Enemy6']
+                    }
+
+                    if self.name in enemies_by_level:
+                        choice = random.choice(enemies_by_level[self.name])
+                        if enemy := EntityFactory.get_entity(choice):
+                            self.entity_list.append(enemy)
+
                     som_aleatorio = random.choice((somMonstro1, somMonstro2, somMonstro3, somMonstro4))
                     som_aleatorio.set_volume(0.1)
                     som_aleatorio.play()
-                    self.entity_list.append(EntityFactory.get_entity(choice))
+
+                if event.type == EVENT_TIMEOUT:
+                    self.timeout -= TIMEOUT_STEP
+                    if self.timeout == 0:
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name == 'Player1':
+                                player_score[0] = ent.score
+                            if isinstance(ent, Player) and ent.name == 'Player2':
+                                player_score[1] = ent.score
+                        return True
+
+                found_player = False
+                for ent in self.entity_list:
+                    if isinstance(ent, Player):
+                        found_player = True
+
+                if not found_player:
+                    return False
 
             self.level_text(14,f'{self.name} - Timeout: {self.timeout / 1000 : .1f}s', color_white,(10,5))
             self.level_text(14, f'fps: {clock.get_fps() : .0f}', color_white,(10, WIN_HEIGHT - 35))
